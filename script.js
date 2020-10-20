38e194114ea00dc103d9fe7ce5ee1ae991d2ded6
Elvin Nunez <eanunez25@gmail.com>
2020-10-20 01:07:54 -0500

    started on website layout

---
+++ script.js
@@ -0,0 +1,11 @@
+let myLibrary = [];
+
+function Book(title, author, pages, genre, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.genre = genre;
+  this.read = read;
+}
+
+function addBookToLibrary
